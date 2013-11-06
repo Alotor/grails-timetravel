d71e07c495f6ac5604c847566814f5f44b9fd53c
Alonso Torres <alonso.torres@kaleidos.net>
2013-11-06 20:56:27 +0100

    Added test plugin config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,20 +10,14 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     legacyResolve true // whether to do a secondary resolve on plugin installation, not advised but set here for backwards compatibility
+
     repositories {
         grailsCentral()
         mavenCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
+
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
         // runtime 'mysql:mysql-connector-java:5.1.18'
     }
 
@@ -31,6 +25,14 @@ grails.project.dependency.resolution = {
         build(":tomcat:$grailsVersion",
               ":release:2.2.0",
               ":rest-client-builder:1.0.3") {
+            export = false
+        }
+
+        test (":spock:0.7", ":code-coverage:1.2.6") {
+            export = false
+        }
+
+        compile (":guard:1.0.7") {
             export = false
         }
     }
