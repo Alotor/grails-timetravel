530a529031c231d3aa77ee6b6c8481d1203325b1
Burt Beckwith <burt@burtbeckwith.com>
2013-11-17 16:46:16 -0500

    added the release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,5 +24,9 @@ grails.project.dependency.resolution = {
         compile (":guard:1.0.7") {
             export = false
         }
+
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
     }
 }
